40ff08977d73d25e0922a06bf047362cba6d3524
carsonchapman1123 <carsonchapman1123@gmail.com>
2026-04-22 23:34:34 -0700

    Added test IPC

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron/main')
+
 const path = require('node:path')
 
 const createWindow = () => {
@@ -9,10 +10,9 @@ const createWindow = () => {
       preload: path.join(__dirname, 'preload.js')
     }
   })
-
   win.loadFile('index.html')
 }
-
 app.whenReady().then(() => {
+  ipcMain.handle('ping', () => 'pong')
   createWindow()
 })
